ad4169b8ecab30bcf602ef475323978d7bf4da1c
Peter Yao <peyao@protonmail.ch>
2017-04-08 16:13:36 -0700

    Solved 2

--- QualificationRound2017/b/peyao/2.js
+++ QualificationRound2017/b/peyao/2.js
@@ -3,10 +3,12 @@
 
 const readline = require('readline');
 const fs = require('fs');
+const bigInt = require("big-integer");
 const rl = readline.createInterface({
   input: fs.createReadStream('2.txt'),
-  // output: process.stdout
+  // output: fs.createWriteStream('2.out')
 });
+const outputStream = fs.createWriteStream('2.out');
 
 let lineNum = 0;
 let totalTestCases;
@@ -19,11 +21,12 @@ rl.on('line', (line) => {
   }
 
   let inputParams = processLine(line);
-  console.log(inputParams);
+  // console.log(inputParams);
   let result = solve(inputParams.N);
 
   // TODO: Output to fs instead of console.log
-  console.log(`Case #${lineNum}: ${result}\n`);
+  console.log(`Case #${lineNum}: ${result}`);
+  outputStream.write(`Case #${lineNum}: ${result}\n`);
   lineNum++;
 });
 
@@ -39,11 +42,20 @@ function solve(N) {
 }
 
 function solveNaive(N) {
-  // Convert string N into array
-  let nArray = N.split('');
-  
-  // Check if array is sorted
-  return isSorted(nArray);
+  let lastTidy;
+  console.log('>>> parseInt(N): ', parseInt(N));
+  for (let i = bigInt(N); i.geq(0); i = i.minus(1)) {
+    // Convert number into string array
+    let elArray = (i + '').split('');
+    console.log('>>> elArray: ', elArray);
+    
+    // Check if array is sorted
+    if (isSorted(elArray)) {
+      return i;
+    } else if (i === 0) {
+      return 0;
+    }
+  }
 }
 
 function isSorted(array) {
