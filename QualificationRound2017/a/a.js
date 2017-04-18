47a445824caa012d4c0152e2a0fcfba837f6df45
sashadt <sasha@datatorrent.com>
2017-04-18 02:10:18 -0700

    Adding Qual A

--- QualificationRound2017/a/a.js
+++ QualificationRound2017/a/a.js
@@ -5,74 +5,48 @@
 // A.  Oversized Pancake Flipper
 // Author: Sasha Parfenov
 
+/* jshint esversion: 6, node: true */
+"use strict";
 
-var glob = require('glob-fs')({ gitignore: true });
-var fs = require("fs");
-var readline = require('readline');
-
-// Arguments processing
-var argv = require('yargs')
-  .usage('Usage: $0 <command> [options]')
-  .example('$0 --verbose < input.txt > output.txt', 'solve the input with verbose output')
+const argv = require('yargs')
+  .usage('Usage: $0 [options] < problem.in > problem.out')
+  .example('$0 -vv < input.txt > output.txt', 'solve the input with verbose output')
   .count('v')
   .alias('v', 'verbose')
   .help('h')
   .alias('h', 'help')
   .argv;
- 
 var VERBOSE_LEVEL = argv.verbose;
- 
-function INFO()  { VERBOSE_LEVEL >= 1 && console.log.apply(console, arguments); }
-function DEBUG() { VERBOSE_LEVEL >= 2 && console.log.apply(console, arguments); }
+function INFO()  { if (VERBOSE_LEVEL >= 1) console.log.apply(console, arguments); }
+function DEBUG() { if (VERBOSE_LEVEL >= 2) console.log.apply(console, arguments); }
 
-DEBUG("Arguments (argv)", argv);
+const rw = require('rw');
+let inputLines = rw.readFileSync("/dev/stdin", "utf8").split('\n');
+const BitArray = require('node-bitarray')
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
+
+let T = parseInt(inputLines[0]);
+let problems = inputLines.slice(1).map((line) => {
+
+  return {
+    s: new BitArray(line.split(' ')[0].replace(/\+/g, '1').replace(/-/g, '0')),
+    k: parseInt(line.split(' ')[1])
+  }
+});
+
+INFO("Input\n-----\n" + inputLines.join('\n'));
+    
+problems.forEach((problem, i) => {
+  DEBUG("Current BitArray: ", problem.s.toString(), "count(+):", problem.s.count());
+  DEBUG("Solving case", (i + 1), ":", JSON.stringify(problem));
+  let result = solve(problem);
+  console.log("Case #" + (i + 1) + ": " + result);
 });
 
 
-var inputLines = [];
-var testCases = [];
-var testCaseCount = -1;
+function solve(problem) {
 
-// Process input from stdin
-rl.on('line', function(line){
-  inputLines.push(line);
-  if (testCaseCount < 0) {
-    testCaseCount = parseInt(line);
-  } else {
-    testCases.push({
-      s: line.split(' ')[0],
-      k: parseInt(line.split(' ')[1])
-    });
-  }
-}).on('close', function(){
 
-  if (testCaseCount !== testCases.length) {
-    console.log("ERROR: Bad number of test cases.  Expected: ", testCaseCount, " Actual: ", testCases.length);
-    console.log("Test Cases: ", JSON.stringify(testCases, null, '  '));
-    process.exit(1);
-  }
-
-  INFO("Input")
-  INFO("-----")
-  INFO(inputLines.join('\n'));
-  INFO("");
-  INFO("Output")
-  INFO("------")
-
-  for(var i = 0; i < testCases.length; i++){
-    var answer = solve(testCases[i]);
-    console.log("Case #" + (i + 1) + ":", answer);
-  }
-
-});
-
-// Solve individual test case
-function solve(testCase) {
 
   return "IMPOSSIBLE";
 }
